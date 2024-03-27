40e6c166a8736f935fa1e1ba53a8da6b0aa85cd7
Lazer430 <fasihabdullah430@gmail.com>
2024-03-27 17:53:46 +0500

    initial code

---
+++ app/app.py
@@ -0,0 +1,31 @@
+# backend/app.py
+from flask import Flask, request, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+mongoDB_port = 27017
+client = MongoClient(f"mongodb://mongodb:{mongoDB_port}/")  # mongodb is the hostname of MongoDB container
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data_recieved = {
+        'name': request.form.get('name'),
+        'email': request.form.get('email')
+    }
+    db = client['webapp_db']
+    collection = db['user_data_collection']
+    result = collection.insert_one(data_recieved)
+    status = ''
+    if result.acknowledged:
+        status = 'Data Submitted Successfully, id: ' + str(result.inserted_id)
+    else:
+        status = 'Data Submission Failed, error: 500'
+
+    return render_template('index.html', status=status)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
